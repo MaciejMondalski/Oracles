8e21c5a2b0fc05508f365a98fa37c5cdc500a515
Maciej Mondalski <maciej.mondalski@gmail.com>
2021-01-13 20:19:26 +0100

    oracle contract update

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -14,4 +14,24 @@ contract Oracle {
 		admin = _admin;
 	}
  
+function updateReporter(address reporter, bool isReporter) external {
+	require(msg.sender == admin, 'only admin');
+	reporters[reporter] = isReporter;
+}
+
+function updateData(bytes32 key, uint payload) external {
+	require(reporters[msg.sender] == true, 'only reporters');
+	data[key] = Data(block.timestamp, payload);
+}
+
+function getData(bytes32 key)
+external
+view
+returns(bool result, uint date, uint payload) {
+	if(data[key].date == 0) {
+		return(false, 0, 0);
+	}
+	return(true, data[key].date, data[key].payload);
+}
+
 }

125e14508308e32bc74b35b2823c3fdba27e8cc2
Maciej Mondalski <maciej.mondalski@gmail.com>
2021-01-13 20:31:36 +0100

    oracle interface update

--- contracts/IntOracle.sol
+++ contracts/IntOracle.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
+
+interface IntOracle {
+    function getData(bytes32 key)
+        external
+        view
+        returns (
+            bool result,
+            uint256 date,
+            uint256 payload
+        );
+}
